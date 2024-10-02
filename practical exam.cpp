6a0fd464a60e93d34d23314494cde6929e140172
Catillo,John Kenneath G. <qjkgcastillo@tip.edu.ph>
2024-10-02 12:47:24 +0800

    Add files via upload

---
+++ practical exam.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i=-5;
+	int j=4;
+	
+	for(i=-5;i<=j;i++)
+	{
+		cout<<"sum of numbers less than 5=1+"<<i<<endl;
+		
+	}
+	return 0;
+	 
+	 
+	 
+}
