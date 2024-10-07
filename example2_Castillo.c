f1e7fd17a697b562eeaec5f7f8353aef6ce6855d
Catillo,John Kenneath G. <qjkgcastillo@tip.edu.ph>
2024-10-07 11:06:40 +0800

    Add files via upload

---
+++ example2_Castillo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main ()
+{
+    printf("the value of 7 is: %f\n", 7.0);
+
+    printf("the value of eight aand a  half is: %float\n", 8.5);
+    return 0;
+}
---
+++ example3_Castillo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+{
+  float halfvalue =0.5;
+  float pivalue = 3.12159265;
+  printf("the value of half is: %f\n", halfvalue);
+  printf("the value of pi is: %f\n", pivalue);
+  return 0;
+  
+}
