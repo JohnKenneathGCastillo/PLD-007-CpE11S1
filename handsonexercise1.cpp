ba12d9ef36765ab9c25be816a08bb2aa50cf34c3
Catillo,John Kenneath G. <qjkgcastillo@tip.edu.ph>
2024-10-14 11:16:22 +0800

    Add files via upload

---
+++ handsonexercise1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "For Loop 1 (1 to 10): ";
+    for (int i1 = 1; i1 <= 10; i1++) {
+        cout << i1 << " ";
+    }
+    cout << endl;
+    
+    cout << "                   " << endl;
+
+    
+    cout << "For Loop 2 (100 to 1): ";
+    for (int i2 = 100; i2 >= 1; i2--) {
+        cout << i2 << " ";
+    }
+    cout << endl;
+    
+    cout << "                   " << endl;
+
+    
+    cout << "For Loop 3 (7 to 77 by 7s): ";
+    for (int i3 = 7; i3 <= 77; i3 += 7) {
+        cout << i3 << " ";
+    }
+    cout << endl;
+    
+    cout << "                   " << endl;
+
+    
+    cout << "For Loop 4 (20 to 2 by 2s): ";
+    for (int i4 = 20; i4 >= 2; i4 -= 2) {
+        cout << i4 << " ";
+    }
+    cout << endl;
+    
+    cout << "                   " << endl;
+
+    
+    cout << "For Loop 5 (2 to 20 by 3s): ";
+    for (int i5 = 2; i5 <= 20; i5 += 3) {
+        cout << i5 << " ";
+    }
+    cout << endl;
+    
+    cout << "                   " << endl;
+
+    
+    cout << "For Loop 6 (99 to 0 by 11s): ";
+    for (int i6 = 99; i6 >= 0; i6 -= 11) {
+        cout << i6 << " ";
+    }
+    
+
+    return 0;
+}
+
